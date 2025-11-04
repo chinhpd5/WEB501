d9fa2e1c0c52a7440167ea3ae1b269c36d5bac20
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-11-04 20:17:23 +0700

    optional chaining, nullish

--- lesson01/main.js
+++ lesson01/main.js
@@ -47,7 +47,10 @@ const total = (a,b=1) => a+b
 
 // Destructing
 const myArray = [1,2,3,4];
-const [a,,c,d] = myArray;
+const [a,...restArr] = myArray;
+// console.log(a);
+// console.log(restArr);
+
 
 // console.log(a); // 1
 // // console.log(b); // undefined
@@ -62,14 +65,74 @@ const myObject = {
   }
 }
 
-var {name : parentName, age, child: {name}} = myObject
-console.log(parentName);
-console.log(age);
-console.log(name);
+var {name, ...restObj} = myObject
+// console.log(name);
+// console.log(restObj);
+// console.log(name);
 
 
-// rest
+// rest: phần còn lại ...
+const sum = (a,...rest) => {
+  let total = a;
+  rest.forEach(item => {
+    total+=item
+  })
+  return total
+}
+// console.log(sum(1,2,3));
+// console.log(sum(1,2,3,4,5));
+// console.log(sum(1,2,3,4,5,6,7,8));
 
-// spread
+
+// spread: giải ...
+const arr1 = [1,2];
+const arr2 = [3,4];
+const totalArr = [...arr1, ...arr2];
+// console.log(totalArr);
+
+const obj1 = {
+  name: 'chinhpd5'
+}
+
+const obj2 = {
+  age: 20
+}
+
+// console.log({...obj1, ...obj2});
+
+// optional chaining (?.)
+
+const myObj = {
+  name: 'chinhpd5'
+}
+
+console.log(myObj.age);
+// if(myObj.age){
+//   console.log(myObj.age.x); // undefined
+// }
+  console.log(myObj.age?.x); // undefined
+
+// logic sau
+// console.log(123);
+
+// nullish ?? undefined hoặc null
+
+// ||
+
+// console.log(0 || 1); // &&
+// console.log(1 && 'a');
+
+let z = 0;
+let y = z ?? 'default'; // ??
+// || loại bỏ các giá trị sai bao gồm 0
+// ?? loại bỏ undefined và null (có nhận giá trị 0);
+console.log(y);
+
+
+
+
+
+
+
 
 
