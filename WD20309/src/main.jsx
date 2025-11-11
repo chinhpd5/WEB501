905bfca45b920839ac4018dc99fc65a2555eed22
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-11-11 23:00:35 +0700

    update

--- WD20309/src/main.jsx
+++ WD20309/src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 function MyComponent(){
+  const info = {
+    name: "chinhpd5",
+    age: 20,
+    gender: true
+  }
 
   return (
     <div>
       <h1>Xin chào các bạn</h1>
-      <h2>Chinhpd5</h2>
+      <h2>Họ và tên: {info.name}</h2>
+      <h3>Tuổi: {info.age}</h3>
+      <h4>Giới tính: {info.gender ? 'Nam' : 'Nữ'}</h4>
     </div>
   )
 }
@@ -15,6 +22,6 @@ function MyComponent(){
 createRoot(document.getElementById('root')).render(
   <div>
     <MyComponent></MyComponent>
-    <MyComponent/>
+    {/* <MyComponent/> */}
   </div>
 )
