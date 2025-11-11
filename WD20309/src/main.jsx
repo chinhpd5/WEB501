888ae43cd3d545681fc2acf812a8de98aabc63a1
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-11-11 19:37:13 +0700

    my component

--- WD20309/src/main.jsx
+++ WD20309/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
+function MyComponent(){
+
+  return (
+    <div>
+      <h1>Xin chào các bạn</h1>
+      <h2>Chinhpd5</h2>
+    </div>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <div>
+    <MyComponent></MyComponent>
+    <MyComponent/>
+  </div>
 )
