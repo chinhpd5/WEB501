3b0c08b4775c0e4993431bc76e30109da3416948
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-11-13 19:04:05 +0700

    props

--- WD20309/src/App.jsx
+++ WD20309/src/App.jsx
@@ -1,8 +1,21 @@
+function ShowName(props) {
+  console.log(props);
+  return (
+    <div>
+      <h1>Xin chào {props.name} </h1>
+      <h2>Tuổi: {props.age}</h2>
+    </div>
+  )
+}
+
+
 function App() {
-  
 
   return (
-    <h1 className="text-center text-danger">Chinhpd5</h1>
+    <div>
+      <ShowName name="chinhpd5" age="20"/>
+      <ShowName name="Phí Đức Chính" age="21"/>
+    </div>
   )
 }
 
