80fb0e4220c080332e950e1421f124536b5ce17a
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-11-13 19:18:11 +0700

    props

--- WD20309/src/App.jsx
+++ WD20309/src/App.jsx
@@ -1,20 +1,18 @@
-function ShowName(props) {
-  console.log(props);
-  return (
-    <div>
-      <h1>Xin chào {props.name} </h1>
-      <h2>Tuổi: {props.age}</h2>
-    </div>
-  )
-}
+import Product from "./component/Product"
 
 
 function App() {
+  const data = {
+    name: "Chuột không dây Logitech M331",
+    price: 350000,
+    category: "Phụ kiện máy tính",
+    inStock: true,
+    image: "https://images.squarespace-cdn.com/content/v1/53883795e4b016c956b8d243/1671853906962-RV08WWNIS1LTNE453MOX/Artboard%2B2.jpg?format=750w"
+  }
 
   return (
     <div>
-      <ShowName name="chinhpd5" age="20"/>
-      <ShowName name="Phí Đức Chính" age="21"/>
+      <Product/>
     </div>
   )
 }
