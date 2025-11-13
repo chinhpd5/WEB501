46422c84041cec28e0e1900b6b31a1598571a29f
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-11-13 18:43:28 +0700

    thêm bootstrap

--- WD20309/src/main.jsx
+++ WD20309/src/main.jsx
@@ -2,26 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-function MyComponent(){
-  const info = {
-    name: "chinhpd5",
-    age: 20,
-    gender: true
-  }
-
-  return (
-    <div>
-      <h1>Xin chào các bạn</h1>
-      <h2>Họ và tên: {info.name}</h2>
-      <h3>Tuổi: {info.age}</h3>
-      <h4>Giới tính: {info.gender ? 'Nam' : 'Nữ'}</h4>
-    </div>
-  )
-}
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 createRoot(document.getElementById('root')).render(
-  <div>
-    <MyComponent></MyComponent>
-    {/* <MyComponent/> */}
-  </div>
+  <StrictMode>
+    <App/>
+  </StrictMode>
 )
