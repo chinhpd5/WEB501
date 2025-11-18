55b488f778c54798c83d37e1591f796d10a1f259
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-11-18 19:26:31 +0700

    two way binding

--- WD20309/src/component/ToDoList.jsx
+++ WD20309/src/component/ToDoList.jsx
@@ -1,15 +1,37 @@
 import React, { useState } from 'react'
 
 function ToDoList() {
-
+  const [text, setText] = useState('')
   const [jobs, setJobs] = useState(['Ăn','Ngủ', 'Nghỉ'])
+
+  console.log(text);
+  
+  const handleAdd = () => {
+    setJobs((prev) => {
+      return [...prev, text]; //spread
+    })
+
+    setText('')
+  }
 
   return (
     <div>
+      <input 
+        type="text" 
+        value={text} 
+        onChange={ (e)=>{
+          // console.log(e.target.value);
+          setText(e.target.value)
+        }}
+      />
+      <button onClick={handleAdd} className='btn btn-primary'>Thêm</button>
       <ul>
         {jobs.map((item,index)=>{
           return (
-            <li key={index}>{item}</li>
+            <li key={index}>
+              {item} 
+              <button className="btn btn-danger">Xóa</button>
+            </li>
           )
         })}
       </ul>
