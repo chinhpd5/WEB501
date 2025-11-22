df7544fede769f0a49692026971348d04e03a462
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-11-22 19:56:18 +0700

    tạo Layout

--- WD20309/src/App.jsx
+++ WD20309/src/App.jsx
@@ -2,16 +2,24 @@ import  {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Product from './pages/Product'
 import About from './pages/About'
+import ClientLayout from './layouts/ClientLayout'
 function App() {
 
   return (
     <>
+      
       <Routes>
-        <Route path='/' element={ <Home/> } />
-        <Route path='/product' element={ <Product/> } />
-        <Route path='/about' element={ <About/> } />
+
+        <Route path='/' element={<ClientLayout/>}>
+          <Route path='' element={ <Home/> } />
+          <Route path='product' element={ <Product/> } />
+          <Route path='about' element={ <About/> } />
+        </Route>
+
+        
         <Route path='*' element={ <h1>Not Found</h1> } />
       </Routes>
+
     </>
   )
 }
