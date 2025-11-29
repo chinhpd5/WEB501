02278f3df0738e9d82582f90239374ed868d89f3
chinhpd5 <chinhpd5@fpt.edu.vn>
2025-11-29 18:54:26 +0700

    Router admin

--- WD20309/src/App.jsx
+++ WD20309/src/App.jsx
@@ -4,6 +4,10 @@ import Product from './pages/Product'
 import ProductDetail from './pages/ProductDetail'
 import About from './pages/About'
 import ClientLayout from './layouts/ClientLayout'
+import AdminLayout from './layouts/AdminLayout'
+import ProductList from './pages/admin/product/List'
+import ProductAdd from './pages/admin/product/Add'
+
 function App() {
 
   return (
@@ -16,6 +20,11 @@ function App() {
           <Route path='product' element={ <Product/> } />
           <Route path='product/:id' element={ <ProductDetail/> } />
           <Route path='about' element={ <About/> } />
+        </Route>
+
+        <Route path='/admin' element={<AdminLayout/>}>
+          <Route path='product' element={<ProductList/>}/>
+          <Route path='product/add' element={<ProductAdd/>}/>
         </Route>
 
         
---
+++ WD20309/src/layouts/AdminLayout.jsx
@@ -0,0 +1,33 @@
+import {Link, Outlet} from "react-router-dom"
+
+function AdminLayout() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <nav className="col-lg-2 bg-dark text-white p-3" style={{ minHeight: '100vh'}}>
+          <h4 className="text-center mb-4">Menu</h4>
+          <ul className="nav flex-column">
+            <li className="nav-item mb-2">
+              <Link to="/admin" className="nav-link text-white">Dashboard</Link>
+            </li>
+            <li className="nav-item mb-2">
+              <Link to="/admin/product" className="nav-link text-white">Sản phẩm</Link>
+            </li>
+            <li className="nav-item mb-2">
+              <Link to="/admin/order" className="nav-link text-white">Đơn hàng</Link>
+            </li>
+            <li className="nav-item mb-2">
+              <Link to="/admin/user" className="nav-link text-white">Người dùng</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* Content phải */}
+        <main className="col-lg-10 p-4">
+          <Outlet/>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default AdminLayout
